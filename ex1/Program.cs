8097454cfca9905d82a2bdccffb05837bc03a0aa
Kleo <mmttdd16@gmail.com>
2023-09-07 06:36:50 +0300

    Add void ex1

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,21 +1,33 @@
 ﻿// void int NewRandomNum [] = 
-int number = new Random().Next(10,100);
-System.Console.WriteLine(number);
-int firstDigit = number/10;
-int secondDigit = number%10;
+void FindMaxDigit(int number)
+{   int firstDigit = number/10;
+    int secondDigit = number%10;
 
-System.Console.WriteLine($"{firstDigit},{secondDigit}");
+    System.Console.WriteLine($"{firstDigit},{secondDigit}");
 
-if (firstDigit>secondDigit)
-{
-    System.Console.WriteLine(firstDigit);
+    if (firstDigit>secondDigit)
+    {
+        System.Console.WriteLine(firstDigit);
+    }
+    else if (firstDigit==secondDigit)
+    {
+        System.Console.WriteLine("Числа равны");
+    }
+    else
+    {
+        System.Console.WriteLine(secondDigit);
+    }
 }
-else if (firstDigit==secondDigit)
-{
-    System.Console.WriteLine("Числа равны");
-}
-else
-{
-    System.Console.WriteLine(secondDigit);
-}
+
+int num = new Random().Next(10,100);
+System.Console.WriteLine(num);
+int num1 = new Random().Next(10,100);
+System.Console.WriteLine(num1);
+int num2 = new Random().Next(10,100);
+System.Console.WriteLine(num2);
+
+FindMaxDigit(num);
+FindMaxDigit(num1);
+FindMaxDigit(num2);
+
 
--- ex2/Program.cs
+++ ex2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int number = 99;
+// Random random = new Random(); // Создаем объект Random один раз
+
+// while (true) // Бесконечный цикл, который мы прерываем при достижении нужного условия
+// {
+//     int i = random.Next(10, 100);
+//     if (i >= number)
+//     {
+//         break; // Выходим из цикла, если i больше или равно number
+//     }
+//     System.Console.WriteLine(i);
+// }
+
+    
