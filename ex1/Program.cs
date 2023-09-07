da95e9d60da0c45d46cfb3a0f7c602878f0ddfc3
Kleo <mmttdd16@gmail.com>
2023-09-07 06:38:52 +0300

    q

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,4 +1,16 @@
 ﻿// void int NewRandomNum [] = 
+
+int num = new Random().Next(10,100);
+System.Console.WriteLine(num);
+int num1 = new Random().Next(10,100);
+System.Console.WriteLine(num1);
+int num2 = new Random().Next(10,100);
+System.Console.WriteLine(num2);
+
+FindMaxDigit(num);
+FindMaxDigit(num1);
+FindMaxDigit(num2);
+
 void FindMaxDigit(int number)
 {   int firstDigit = number/10;
     int secondDigit = number%10;
@@ -18,16 +30,4 @@ void FindMaxDigit(int number)
         System.Console.WriteLine(secondDigit);
     }
 }
-
-int num = new Random().Next(10,100);
-System.Console.WriteLine(num);
-int num1 = new Random().Next(10,100);
-System.Console.WriteLine(num1);
-int num2 = new Random().Next(10,100);
-System.Console.WriteLine(num2);
-
-FindMaxDigit(num);
-FindMaxDigit(num1);
-FindMaxDigit(num2);
-
 
