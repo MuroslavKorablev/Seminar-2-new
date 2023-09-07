d649c968ba33620f724a96d56b9904be55319df0
Kleo <mmttdd16@gmail.com>
2023-09-07 06:19:12 +0300

    Finished ex1

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// void int NewRandomNum [] = 
+int number = new Random().Next(10,100);
+System.Console.WriteLine(number);
+int firstDigit = number/10;
+int secondDigit = number%10;
+
+System.Console.WriteLine($"{firstDigit},{secondDigit}");
+
+if (firstDigit>secondDigit)
+{
+    System.Console.WriteLine(firstDigit);
+}
+else if (firstDigit==secondDigit)
+{
+    System.Console.WriteLine("Числа равны");
+}
+else
+{
+    System.Console.WriteLine(secondDigit);
+}
+
